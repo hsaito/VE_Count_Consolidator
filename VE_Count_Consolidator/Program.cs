0d168da7a9e642a3498f4600b72ae2e149836a97
Hideki Saito <hideki@hidekisaito.com>
2017-08-21 01:11:18 -0700

    Project added from dotnet templates

---
+++ VE_Count_Consolidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VE_Count_Consolidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
