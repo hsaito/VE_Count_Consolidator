89fcf7018d8bb7378da3f3f48029aa03fffe58f8
Hideki Saito <hideki@hidekisaito.com>
2017-09-18 21:54:54 -0700

    Removed unused args

--- VE_Count_Consolidator/Program.cs
+++ VE_Count_Consolidator/Program.cs
@@ -12,8 +12,7 @@ namespace VE_Count_Consolidator
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
 
-        // ReSharper disable once UnusedParameter.Global
-        public static int Main(string[] args)
+        public static int Main()
         {
             if (!InitializeLogging()) return -1;
             try
