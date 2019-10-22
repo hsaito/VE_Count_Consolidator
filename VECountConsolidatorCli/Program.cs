69bef708c0a41a3b8e0b3f4771b61958eadefa6c
Hideki Saito <hideki@hidekisaito.com>
2019-10-22 03:07:34 -0700

    Updated to .NET Core 3

--- VECountConsolidatorCli/Program.cs
+++ VECountConsolidatorCli/Program.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Xml;
 using CommandLine;
 using CsvHelper;
-using log4net;
-using log4net.Config;
-using log4net.Repository.Hierarchy;
 using VECountConsolidator;
 
 namespace VECountConsolidatorCli
 {
     internal class Program
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
-
-
         public static int Main(string[] args)
         {
-            if (!InitializeLogging()) return -1;
             try
             {
                 Parser.Default.ParseArguments<Options>(args).WithParsed(
@@ -29,12 +20,13 @@ namespace VECountConsolidatorCli
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex.Message);
+                Console.Error.WriteLine(ex.Message);
                 return -1;
             }
 
             return 0;
         }
+
 
         private static void Generate(Options options)
         {
@@ -79,34 +71,7 @@ namespace VECountConsolidatorCli
             }
             catch (Exception e)
             {
-                Log.Error(e.Message);
-            }
-        }
-
-        /// <summary>
-        ///     Initialize logging
-        /// </summary>
-        private static bool InitializeLogging()
-        {
-            try
-            {
-                // Configuration for logging
-                var log4NetConfig = new XmlDocument();
-
-                using (var reader = new StreamReader(new FileStream("log4net.config", FileMode.Open, FileAccess.Read)))
-                {
-                    log4NetConfig.Load(reader);
-                }
-
-                var rep = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(Hierarchy));
-                XmlConfigurator.Configure(rep, log4NetConfig["log4net"]);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error initializing the logging.");
-                Console.WriteLine(ex.Message);
-                return false;
+                Console.Error.WriteLine(e.Message);
             }
         }
 
