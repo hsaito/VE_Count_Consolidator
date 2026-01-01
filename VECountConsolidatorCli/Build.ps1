5999c270caca6013b14a0f5c2dcad759de00745c
Hideki Saito <hidekis@gmail.com>
2025-12-31 23:43:36 -0800

    Update Dockerfile and project files to target .NET 10.0 and update dependencies

--- VECountConsolidatorCli/Build.ps1
+++ VECountConsolidatorCli/Build.ps1
@@ -1,15 +1,16 @@
 $version = "1.7.0.0"
 $product = 'VE_Count_Consolidator'
+$framework = 'net10.0'
 
-Remove-Item *.zip
+Remove-Item *.zip -ErrorAction SilentlyContinue
 
 $architectures = @("win-x64", "win-x86", "linux-x64", "linux-musl-x64", "linux-arm", "osx-x64")
 
 Foreach ($architecture in $architectures)
 {
     dotnet clean
-    Remove-Item -LiteralPath bin -Recurse
-    Remove-Item -LiteralPath obj -Recurse
-    dotnet publish -c Release -r $architecture /p:PublishSingleFile=true
-    Compress-Archive ./bin/Release/netcoreapp3.0/$architecture/publish/* "$product-$version-$architecture.zip"
+    Remove-Item -LiteralPath bin -Recurse -ErrorAction SilentlyContinue
+    Remove-Item -LiteralPath obj -Recurse -ErrorAction SilentlyContinue
+    dotnet publish -c Release -r $architecture -f $framework /p:PublishSingleFile=true
+    Compress-Archive ./bin/Release/$framework/$architecture/publish/* "$product-$version-$architecture.zip"
 }
